b1465df4b3de3eceeca735030d620a1c1ec640dc
sathia27 <sathia2704@gmail.com>
2014-05-08 22:34:12 +0530

    added label in form and epub file name changes

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,17 +2,16 @@ class BlogPostsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:generate_epub]
   def generate_epub
     ids = params[:ids].split(",")
-    links = []
     if(ids)
       posts = BlogPost.where(:id.in => ids)
-      domain = posts.first.blog.name
-      links = posts.collect(&:blog_url)
+      blog = posts.first.blog
+      domain = blog.name
       domain_folder = "public/ebooks/" + domain
       Dir.mkdir domain_folder unless Dir.exists? domain_folder
       Dir.mkdir domain_folder + "/html" unless Dir.exists? domain_folder + "/html"
       HtmlGenerator.new(posts, domain_folder).generate
       EbookBuilder.new(posts).build
-      send_file "#{Rails.public_path}/ebooks/#{domain}/site.epub", :filename => "site.epub"
+      send_file "#{Rails.public_path}/ebooks/#{domain}/site.epub", :filename => "#{blog.book_name.gsub(" ", "_")}.epub"
     else
       render :json => false
     end
